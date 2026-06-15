1cfae08e05f10379d0c7b88fcc52ce6ae70a2248
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-06-15 12:51:49 +0545

    feat(layout): create layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+// app/layout.tsx
+import React from 'react';
+
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = async ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      
+      <main className="flex-1 px-5">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
